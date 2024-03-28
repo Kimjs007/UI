b328afae6636d1b3c1602b752c82c33be5412549
kimjs007 <kimjs007@dongaeltek.co.kr>
2024-03-28 14:26:12 +0900

    INIT UI

---
+++ IO/SRC_20240326_SIMPLE_1/___del.bat
@@ -0,0 +1,19 @@
+del *.~*
+del *.ddp
+del *.dcu
+del *.dsk
+del *.dsm
+del *.bak
+del *.map
+del *.dpc
+del *.drc
+del *.local
+del *.res
+del *.stat
+del *.identcache
+del *.mes
+rmdir /Q __history/S
+rmdir /Q __recovery/S
+rmdir /Q VIT/S
+rmdir /Q Win32/S
+rmdir /Q Win64/S
